cdcc4bea82db37e4ce52a919ed4647b9eea6ad74
Nima Shoghi <nimashoghi@gmail.com>
2020-04-28 20:12:45 +0000

    More bug fixes

--- src/__tests__/compress.spec.ts
+++ src/__tests__/compress.spec.ts
@@ -1,50 +1,162 @@
-import {compress} from "../compress"
+/* eslint-disable jest/expect-expect */
+import {compress, decompress, toArray, toByteArray} from "../compress"
 
-describe("algorithm", () => {
+describe("compression", () => {
     it("compresses correctly with the presentation exmaple", () => {
         expect(
-            compress(
-                new BigUint64Array([
-                    0xc04039c0n,
-                    0xc04039c8n,
-                    0xc04039d0n,
-                    0xc04039f8n,
-                ]),
+            toArray(
+                compress(
+                    new BigUint64Array([
+                        BigInt(0xc04039c0),
+                        BigInt(0xc04039c8),
+                        BigInt(0xc04039d0),
+                        BigInt(0xc04039f8),
+                    ]),
+                ),
             ),
-        ).toEqual([0xc04039c0, 0x00, 0x08, 0x10, 0x38])
+        ).toEqual([
+            BigInt(0xc04039c0),
+            BigInt(0x00),
+            BigInt(0x08),
+            BigInt(0x10),
+            BigInt(0x38),
+        ])
     })
 
     it("compresses correctly with the h264ref example", () => {
         expect(
-            compress(
-                new BigUint64Array([
-                    0xc00000000n,
-                    0xc0000000bn,
-                    0xc00000003n,
-                    0xc00000001n,
-                    0xc00000004n,
-                    0xc00000000n,
-                    0xc00000003n,
-                    0xc00000004n,
-                ]),
+            toArray(
+                compress(
+                    new BigUint64Array([
+                        BigInt(0xc00000000),
+                        BigInt(0xc0000000b),
+                        BigInt(0xc00000003),
+                        BigInt(0xc00000001),
+                        BigInt(0xc00000004),
+                        BigInt(0xc00000000),
+                        BigInt(0xc00000003),
+                        BigInt(0xc00000004),
+                    ]),
+                ),
             ),
-        ).toEqual([0xc00000000, 0x00, 0x0b, 0x03, 0x01, 0x04, 0x00, 0x03, 0x04])
+        ).toEqual([
+            BigInt(0xc00000000),
+            BigInt(0x00),
+            BigInt(0x0b),
+            BigInt(0x03),
+            BigInt(0x01),
+            BigInt(0x04),
+            BigInt(0x00),
+            BigInt(0x03),
+            BigInt(0x04),
+        ])
     })
 
     it("compresses correctly with the perlbench example", () => {
         expect(
-            compress(
-                new BigUint64Array([
-                    0xc04039c0n,
-                    0xc04039c8n,
-                    0xc04039d0n,
-                    0xc04039d8n,
-                    0xc04039e0n,
-                    0xc04039e8n,
-                    0xc04039f0n,
-                    0xc04039f8n,
-                ]),
+            toArray(
+                compress(
+                    new BigUint64Array([
+                        BigInt(0xc04039c0),
+                        BigInt(0xc04039c8),
+                        BigInt(0xc04039d0),
+                        BigInt(0xc04039d8),
+                        BigInt(0xc04039e0),
+                        BigInt(0xc04039e8),
+                        BigInt(0xc04039f0),
+                        BigInt(0xc04039f8),
+                    ]),
+                ),
             ),
-        ).toEqual([0xc04039c0, 0x00, 0x08, 0x10, 0x18, 0x20, 0x28, 0x30, 0x38])
+        ).toEqual([
+            BigInt(0xc04039c0),
+            BigInt(0x00),
+            BigInt(0x08),
+            BigInt(0x10),
+            BigInt(0x18),
+            BigInt(0x20),
+            BigInt(0x28),
+            BigInt(0x30),
+            BigInt(0x38),
+        ])
+    })
+
+    it("can't compress data that doesn't have locality", () => {
+        expect(
+            toArray(
+                compress(
+                    new BigUint64Array([
+                        BigInt(0xa04039c0),
+                        BigInt(0xb04039c8),
+                        BigInt(0xc04039d0),
+                        BigInt(0xd04039f8),
+                    ]),
+                ),
+            ),
+        ).toEqual([
+            BigInt(0xa04039c0),
+            BigInt(0xb04039c8),
+            BigInt(0xc04039d0),
+            BigInt(0xd04039f8),
+        ])
+    })
+})
+
+const assertDecompression = (buffer: BigUint64Array) => {
+    const out = compress(buffer)
+    const byteArray = toByteArray(out)
+    if (out === undefined || byteArray === undefined) {
+        throw new Error(`This shouldn't happen`)
+    }
+    expect(out).toBeDefined()
+
+    for (let i = 0; i < out.elements.length; ++i) {
+        const expected = buffer[i]
+        const actual = decompress(byteArray, out!.elements[i].tag)
+
+        expect(actual.toString(16)).toBe(expected.toString(16))
+    }
+}
+
+describe("decompression", () => {
+    it("compresses correctly with the presentation exmaple", () => {
+        assertDecompression(
+            new BigUint64Array([
+                BigInt(0xc04039c0),
+                BigInt(0xc04039c8),
+                BigInt(0xc04039d0),
+                BigInt(0xc04039f8),
+            ]),
+        )
+    })
+
+    it("compresses correctly with the h264ref example", () => {
+        assertDecompression(
+            new BigUint64Array([
+                BigInt(0xc00000000),
+                BigInt(0xc0000000b),
+                BigInt(0xc00000003),
+                BigInt(0xc00000001),
+                BigInt(0xc00000004),
+                BigInt(0xc00000000),
+                BigInt(0xc00000003),
+                BigInt(0xc00000004),
+            ]),
+        )
+    })
+
+    it("compresses correctly with the perlbench example", () => {
+        assertDecompression(
+            new BigUint64Array([
+                BigInt(0xc04039c0),
+                BigInt(0xc04039c8),
+                BigInt(0xc04039d0),
+                BigInt(0xc04039d8),
+                BigInt(0xc04039e0),
+                BigInt(0xc04039e8),
+                BigInt(0xc04039f0),
+                BigInt(0xc04039f8),
+            ]),
+        )
     })
 })
--- src/compress.ts
+++ src/compress.ts
@@ -16,27 +16,28 @@ const baseDelta = (
 
 const sizeToLimit = (size: bigint) => {
     switch (size) {
-        case 1n:
-            return 0xffn
-        case 2n:
-            return 0xffffn
-        case 4n:
-            return 0xffffffffn
+        case BigInt(1):
+            return BigInt(0xff)
+        case BigInt(2):
+            return BigInt(0xffff)
+        case BigInt(4):
+            return BigInt(0xffffffff)
+        case BigInt(8):
+            return BigInt("0xffffffffffffffff")
         default:
-            throw new Error()
+            throw new Error(`${size} should be 1, 2, or 4`)
     }
 }
 
 const makeTag = (index: bigint, encoding: bigint, base: bigint) =>
-    (index << 5n) | (encoding << 1n) | (base === 0n ? 0x0n : 0x1n)
-
-const tag = makeTag(23232441n, 0b1001n, 2n)
-tag.toString(2) // ?
+    (index << BigInt(5)) |
+    (encoding << BigInt(1)) |
+    (base === BigInt(0) ? BigInt(0x0) : BigInt(0x1))
 
 const processTag = (tag: bigint) => ({
-    encoding: (tag >> 0x1n) & 0b1111n,
-    index: tag >> 0x5n,
-    isZeroBase: !((tag & 0x1n) === 0b1n),
+    encoding: (tag >> BigInt(0x1)) & BigInt(0b1111),
+    index: tag >> BigInt(0x5),
+    isZeroBase: !((tag & BigInt(0x1)) === BigInt(0b1)),
 })
 
 interface CompressionConfig {
@@ -52,122 +53,139 @@ const compressWith = (
     const baseLimit = sizeToLimit(baseSize)
     const deltaLimit = sizeToLimit(deltaSize)
 
-    const buffer_ = baseDelta(buffer, deltaLimit, baseSize, 0n)
+    const buffer_ = baseDelta(buffer, deltaLimit, deltaSize, BigInt(0))
     const baseIndex = buffer_.findIndex(
-        (element) => element.size !== baseSize && element.value < baseLimit,
+        (element) => element.base === undefined && element.value < baseLimit,
     )
-    if (baseIndex === undefined) {
+    if (baseIndex === -1) {
         return undefined
     }
     buffer_[baseIndex].size = baseSize
     const final = baseDelta(
         buffer_,
         deltaLimit,
-        baseSize,
+        deltaSize,
         buffer_[baseIndex].value,
     )
-    if (!final.every((element) => element.size === deltaSize)) {
+    if (!final.every((element) => element.base !== undefined)) {
         return undefined
     }
     return {
+        base: buffer_[baseIndex].value,
+        baseSize,
         elements: final.map((element, i) => ({
             ...element,
-            tag: makeTag(BigInt(i), encoding, element.base ?? 0n),
+            tag: makeTag(BigInt(i), encoding, element.base ?? BigInt(0)),
         })),
-        size: baseSize + BigInt(final.length) * deltaSize,
+        size:
+            baseSize +
+            final.reduce((acc, element) => acc + element.size, BigInt(0)),
     }
 }
 
 const configs: CompressionConfig[] = [
     {
-        baseSize: 8n,
-        deltaSize: 1n,
-        encoding: 0b0010n,
+        baseSize: BigInt(8),
+        deltaSize: BigInt(1),
+        encoding: BigInt(0b0010),
         name: "base8-delta1",
     },
     {
-        baseSize: 8n,
-        deltaSize: 4n,
-        encoding: 0b0100n,
+        baseSize: BigInt(8),
+        deltaSize: BigInt(4),
+        encoding: BigInt(0b0100),
         name: "base8-delta4",
     },
     {
-        baseSize: 8n,
-        deltaSize: 2n,
-        encoding: 0b0011n,
+        baseSize: BigInt(8),
+        deltaSize: BigInt(2),
+        encoding: BigInt(0b0011),
         name: "base8-delta2",
     },
     {
-        baseSize: 4n,
-        deltaSize: 2n,
-        encoding: 0b0110n,
+        baseSize: BigInt(4),
+        deltaSize: BigInt(2),
+        encoding: BigInt(0b0110),
         name: "base4-delta2",
     },
     {
-        baseSize: 4n,
-        deltaSize: 1n,
-        encoding: 0b0101n,
+        baseSize: BigInt(4),
+        deltaSize: BigInt(1),
+        encoding: BigInt(0b0101),
         name: "base4-delta1",
     },
     {
-        baseSize: 2n,
-        deltaSize: 1n,
-        encoding: 0b0111n,
+        baseSize: BigInt(2),
+        deltaSize: BigInt(1),
+        encoding: BigInt(0b0111),
         name: "base2-delta1",
     },
 ]
 
 const bufferToElement = (buffer: BigUint64Array) =>
     Array.from(new BigInt64Array(buffer)).map((value) => ({
-        size: 4n,
+        size: BigInt(8),
         value,
     }))
 
 const toBytes = (e: bigint, size: bigint) => {
     const a: number[] = []
-    for (let i = 0n; i < size; ++i) {
-        a.push(Number((e << i) & 0xffn))
+    for (let i = BigInt(0); i < size; ++i) {
+        a.push(Number((e >> (i * BigInt(8))) & BigInt(0xff)))
     }
-    return a
+    // return Buffer.from(a)
+    return a.reverse()
 }
 
-const makeOutput = (
-    compressed: NonNullable<ReturnType<typeof compressWith>>,
-) => {
-    const out = new Uint8Array(
-        compressed.elements.flatMap((element) =>
-            toBytes(element.value, element.size),
-        ),
-    )
-    if (out.length !== Number(compressed.size)) {
-        throw new Error("wat")
-    }
-    return out
+interface CompressedOutput {
+    base: bigint
+    baseSize: bigint
+    elements: {
+        base?: bigint
+        size: bigint
+        tag: bigint
+        value: bigint
+    }[]
+    encoding: bigint
+    size: bigint
 }
 
-export const compress = (buffer: BigUint64Array) => {
+export const tryCompress = (
+    buffer: BigUint64Array,
+): CompressedOutput | undefined => {
     const elements = bufferToElement(buffer)
-    if (buffer.every((x) => x === 0n)) {
-        return makeOutput({
+    if (buffer.every((x) => x === BigInt(0))) {
+        return {
+            base: BigInt(0),
+            baseSize: BigInt(1),
             elements: [
-                {base: 0n, size: 1n, tag: makeTag(0n, 0b0000n, 0n), value: 0n},
+                {
+                    base: BigInt(0),
+                    size: BigInt(1),
+                    tag: makeTag(BigInt(0), BigInt(0b0000), BigInt(0)),
+                    value: BigInt(0),
+                },
             ],
-            size: 1n,
-        })
+            encoding: BigInt(0b0000),
+            size: BigInt(1),
+        }
     }
 
     if (buffer.every((x) => x === buffer[0])) {
-        return makeOutput({
+        return {
+            base: BigInt(0),
+            baseSize: BigInt(8),
             elements: [
                 {
                     base: buffer[0],
-                    size: 4n,
-                    tag: makeTag(0n, 0b0001n, 0n),
+                    size: BigInt(8),
+                    tag: makeTag(BigInt(0), BigInt(0b0001), BigInt(0)),
                     value: buffer[0],
                 },
             ],
-            size: 4n,
-        })
+            encoding: BigInt(0b0000),
+            size: BigInt(8),
+        }
     }
 
     const compressed = configs
@@ -176,7 +194,11 @@ export const compress = (buffer: BigUint64Array) => {
             if (value === undefined) {
                 return undefined
             }
-            return {...value, name: config.name}
+            return {
+                ...value,
+                encoding: config.encoding,
+                name: config.name,
+            }
         })
         .reduce((prev, curr) =>
             (prev?.size ?? Number.MAX_SAFE_INTEGER) >
@@ -191,18 +213,82 @@ export const compress = (buffer: BigUint64Array) => {
     }
 
     console.log(compressed.name)
-    return makeOutput(compressed)
+    return compressed
 }
 
-export const decompress = (buffer: BigUint64Array, tag: bigint) => {
+export const compress = (buffer: BigUint64Array): CompressedOutput =>
+    tryCompress(buffer) ?? {
+        base: BigInt(0),
+        baseSize: BigInt(0),
+        elements: [...buffer].map((value) => ({
+            base: undefined,
+            size: BigInt(0x8),
+            tag: makeTag(BigInt(0), BigInt(0b1111), BigInt(0)),
+            value,
+        })),
+        encoding: BigInt(0b1111),
+        size: BigInt(buffer.length) * BigInt(8),
+    }
+
+export const toArray = (compressed: ReturnType<typeof compress>) => {
+    const out = compressed.elements.map((element) => element.value)
+    if (
+        new Uint8Array(
+            compressed.elements.flatMap((element) =>
+                toBytes(element.value, element.size),
+            ),
+        ).length !== Number(compressed.size - compressed.baseSize)
+    ) {
+        throw new Error("wat")
+    }
+    return [
+        ...(compressed.encoding === BigInt(0b1111) ? [] : [compressed.base]),
+        ...out,
+    ]
+}
+
+export const toByteArray = (compressed: ReturnType<typeof compress>) => {
+    if (compressed === undefined) {
+        return undefined
+    }
+
+    const out = Buffer.from([
+        ...(compressed.encoding === BigInt(0b1111)
+            ? []
+            : toBytes(compressed.base, compressed.baseSize)),
+        ...compressed.elements.flatMap((element) =>
+            toBytes(element.value, element.size),
+        ),
+    ])
+
+    if (out.length !== Number(compressed.size)) {
+        throw new Error("wat")
+    }
+    return out
+}
+
+const arrayBufferToBigInt = (buf: ArrayBufferLike) => {
+    const hex: string[] = []
+
+    for (const i of new Uint8Array(buf)) {
+        let h = i.toString(16)
+        if (h.length % 2) {
+            h = "0" + h
+        }
+        hex.push(h)
+    }
+
+    return BigInt("0x" + (hex.join("") || "0"))
+}
+
+export const decompress = (buffer: ArrayBufferLike, tag: bigint) => {
     const {encoding, index, isZeroBase} = processTag(tag)
 
-    const u64buffer = new BigUint64Array(buffer)
     switch (encoding) {
-        case 0b0000n:
+        case BigInt(0b0000):
             return 0
-        case 0b0001n:
-            return u64buffer[0]
+        case BigInt(0b0001):
+            return new BigUint64Array(buffer.slice(0, 8))[0]
         default:
             const config = configs.find(
                 (config) => config.encoding === encoding,
@@ -212,10 +298,13 @@ export const decompress = (buffer: BigUint64Array, tag: bigint) => {
             }
 
             const base = isZeroBase
-                ? 0n
-                : BigInt(buffer.subarray(0, Number(config.baseSize)))
+                ? BigInt(0)
+                : arrayBufferToBigInt(buffer.slice(0, Number(config.baseSize)))
             const start = config.baseSize + index * config.deltaSize
             const end = start + config.deltaSize
-            return base + BigInt(buffer.subarray(Number(start), Number(end)))
+            const delta = arrayBufferToBigInt(
+                buffer.slice(Number(start), Number(end)),
+            )
+            return base + delta
     }
 }
